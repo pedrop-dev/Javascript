725400af84d6772d8f471a626af792dd6a8c1bf3
Pedro Henrique <92335472+pedrop-dev@users.noreply.github.com>
2023-04-16 15:52:45 -0300

    new changes

---
+++ javascript-eb/aula015/script.js
@@ -0,0 +1,32 @@
+/*
+Timers
+setTimeOut(função, delay)
+setInterval(função, delay) , clearInterval
+*/
+
+//const delay = 24 * 60 * 60 * 1000 Milisegundos
+//Sempre escrever o tempo em uma variável
+const delay = 1000
+
+function print(value1, value2, n) {
+    console.log(value1, value2, n)
+}
+setTimeout(print, delay, 'Pedro', 'JavaScript', 'N')
+
+//////////////////////////////////////
+
+const delay2 = 1000
+let counter = 0
+
+let intervalId = null
+
+intervalId = setInterval(function() {
+    console.log('Pedro', counter)
+    counter++
+
+    if (counter > 10) {
+        clearInterval(intervalId)
+    }
+}, delay2)
+
+console.log('intervalId', intervalId)
